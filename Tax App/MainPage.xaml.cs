3e645c666632eecfd6355b13cfa9f15fd9feb7a1
Ryan Doiron <doir0008@algonquinlive.com>
2017-06-25 17:14:49 -0400

    Add project files.

---
+++ Tax App/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Tax_App
+{
+    /*
+     * An app that calculates sales tax
+    */
+    public sealed partial class MainPage : Page
+    {
+        Tax tax;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            // Create new obj ref
+            tax = new Tax();
+        }
+
+        private void purchaseAmount_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // set text to empty when got focus
+            purchaseAmount.Text = string.Empty;
+        }
+
+        private void purchaseAmount_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // set text to calculated amount when lost focus
+            purchaseAmount.Text = tax.purchasePrice;
+        }
+
+        private void purchaseAmount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            doCalculation();
+        }
+
+        private void radioButton_Click(object sender, RoutedEventArgs e)
+        {
+            doCalculation();
+        }
+
+        private void doCalculation()
+        {
+            // determine which radio button is selected
+            RadioButton selectedTax = mainStackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+            // grab the tag value and store it
+            string selectedTaxAmount = selectedTax.Tag.ToString();
+            // calculate the taxes
+            tax.Calculate(purchaseAmount.Text, selectedTaxAmount);
+            // set the text labels to new values
+            taxAmount.Text = tax.taxAmount;
+            totalAmount.Text = tax.totalAmount;
+        }
+    }
+}
---
+++ Tax App/Tax.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Created by Ryan Doiron (doir0008@algonquinlive.com)
+ *
+ */
+namespace Tax_App
+{
+    class Tax
+    {
+        public string purchasePrice;
+        public string taxAmount;
+        public string totalAmount;
+
+        // init the Tax obj and variables
+        public Tax()
+        {
+            purchasePrice = string.Empty;
+            taxAmount = string.Empty;
+            totalAmount = string.Empty;
+        }
+
+        // Calculate taxes method
+        public void Calculate(string price, string salesTax)
+        {
+            double numericPrice = 0.0;
+            double numericTax = 0.0;
+            double numericTotal = 0.0;
+            double numericTotalTax = 0.0;
+            bool success = false;
+
+            // Parse the incoming string, removing the $ and if successful set to true
+            success = double.TryParse(price.Replace('$', ' '), out numericPrice);
+            // If parsed string is good, we proceed
+            if(success)
+            {
+                // Parse the incoming string and if successful set to true
+                success = double.TryParse(salesTax, out numericTax);
+                // If parsed string is good, we proceed
+                if (success)
+                {
+                    // perform tax calcs
+                    numericTotalTax = numericPrice * numericTax;
+                    numericTotal = numericTotalTax + numericPrice;
+                    // set text values formatted to currency strings
+                    purchasePrice = numericPrice.ToString("c");
+                    taxAmount = numericTotalTax.ToString("c");
+                    totalAmount = numericTotal.ToString("c");
+                }
+            }
+        }
+    }
+}
